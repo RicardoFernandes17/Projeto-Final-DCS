c3eeb0f8ecb4208117c7f74e6502c2b61983379a
Ricardo <a18476@alunos.ipca.pt>
2021-01-19 21:36:58 +0000

    Se estiver na HomePage aparece opções de scroll dentro da page na Navbar e na Sidenav.

--- Front-end/src/components/Sidebar/index.js
+++ Front-end/src/components/Sidebar/index.js
@@ -2,16 +2,27 @@ import React from 'react'
 import {SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SidebarLinkR, SideBtnWrap, SidebarRoute} from './SidebarElements'
 
 const Sidebar = ({isOpen, toggle}) => {
+    window.homepagecheck = function() {
+        var check = false;
+        if(document.location.pathname === "/"){
+          check=true;
+          }
+        return check;
+      }
+
     return (
         <SidebarContainer isOpen={isOpen} onClick={toggle}>
-            <Icon onCLicl={toggle}>
+            <Icon onCLick={toggle}>
                 <CloseIcon />
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
+                {window.homepagecheck() ?(<>
                     <SidebarLink to="braga" onClick={toggle}>Braga</SidebarLink>
                     <SidebarLink to="porto" onClick={toggle}>Porto</SidebarLink>
                     <SidebarLink to="lisboa" onClick={toggle}>Lisboa</SidebarLink>
+                    </>
+                ):(<></>)}
                     <SidebarLinkR to="/suggestedroutes" onClick={toggle}>Rotas Sugeridas</SidebarLinkR>
                     <SidebarLinkR to="/routes" onClick={toggle}>Rotas</SidebarLinkR>
                     <SidebarLinkR to="/signup" onClick={toggle}>Sign Up</SidebarLinkR>
